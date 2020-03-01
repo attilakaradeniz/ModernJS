db34b6b754611991ae195ff6f0ad3f8495130206
itsumogakusei <attilakaradeniz@gmail.com>
2020-03-01 17:57:31 +0100

    NULL & undefined & boolean

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,28 +1,7 @@
-// 00:46:16 - template strings
+// 01:02:20  NULL & undefined
+//let age;
+let age = null;
 
-const title = 'Best reads of 2019';
-const author = 'Mario';
-const likes = 30;
-
-// concatenation way
-//let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
-//console.log(result);
-
-// template strings bzw. => template literal
-//let result = `The blog calles ${title} by ${author} has ${likes} likes`;
-//console.log(result);
-
-// creating html templates
-let html = `
-    <h2>${title}</h2>
-    <p>By ${author}</p>
-    <span>This blog has ${likes} likes</span>
-`; 
- 
-console.log(html);
-
-let spanTemplate = $("#forTemplate");
-
-spanTemplate.append(html);
-
+console.log(age , age + 3, `the age is ${age}`);
+console.log(true, false, 'true', 'false');
 
