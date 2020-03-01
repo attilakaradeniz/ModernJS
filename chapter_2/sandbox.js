a8dd2162c893e024a39eeb14779fa707b5fae8a5
itsumogakusei <attilakaradeniz@gmail.com>
2020-03-01 13:57:40 +0100

    added template string & dynamic html

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,10 +1,28 @@
-// 00:36:20  numbers
+// 00:46:16 - template strings
 
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
 
+// concatenation way
+//let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
+//console.log(result);
 
+// template strings bzw. => template literal
+//let result = `The blog calles ${title} by ${author} has ${likes} likes`;
+//console.log(result);
 
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`; 
+ 
+console.log(html);
 
+let spanTemplate = $("#forTemplate");
 
-
+spanTemplate.append(html);
 
 
