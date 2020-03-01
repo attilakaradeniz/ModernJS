663a481f3ce82d82771d92eb215ae3f395d55e2a
itsumogakusei <attilakaradeniz@gmail.com>
2020-03-01 21:01:55 +0100

    js cleaned before ninja vid3

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,28 +1,7 @@
-// 01:18:50  Type conversion
-let score = '100';
+$(document).ready(function(){
+    /////////
 
-// console.log(score + 1);
-// console.log(parseInt(score) + 1); // parseInt
+    
 
-// console.log(typeof score);
-
-// score = Number(score);
-
-// console.log(score + 1);
-// console.log(typeof score);
-
-// let result = Number('hellori');
-// let result = String(50);
-
-
-// truthy & falsy
-// let result = Boolean(100);  
-let result = Boolean(0); // falsy
-result = Boolean('0'); // truthy
-result = Boolean(''); // falsy
-
-
-console.log(result, typeof result);
-
-
-
+    /////////
+});
--- trackLessons.js
+++ trackLessons.js
@@ -1,5 +1,7 @@
 /*
 
+// https://www.youtube.com/watch?v=FhguwBJeqWs&list=PL4cUxeGkcC9haFPT7J25Q9GRB_ZkFrQAc&index=2
+
 $(document).ready(function(){
 
     let age = 25;
@@ -64,13 +66,199 @@ console.log(result, fullName);
 
 let index = email.indexOf('@');
 console.log(index);
-
-
-    
-    
-
-
 });
+
+//////////////////////////////////////
+// 00:31:12
+
+// common string methods
+
+let email = 'itsumogakusei@gmail.com';
+
+// let result = email.lastIndexOf('a');
+// let result = email.slice(0, 5);  // from to
+//let result = email.substr(4, 5);
+// let result = email.replace('m', 'w'); // prints out => itsuwogakusei@gmail.com
+// let result = email.replace('a', 'o'); // replace the first one it comes to
+
+console.log(result);
+
+/////////////////////////////////////////
+// 00:36:20  numbers
+let radius = 10;
+const pi = 3.14;
+
+//console.log(radius, pi);
+// math operators  +, -, *, ** (power of), % (remainder)
+// let result = pi * radius**2;
+
+// the order of operation - B I D M A S
+
+//let result = 5 * (10-3)**2;
+// console.log(result);
+
+
+let likes = 10;
+
+// likes++;
+// likes--;
+// likes += 10;
+// likes -= 10;
+// likes *= 2;
+// likes /= 2;
+
+// console.log(likes);
+
+// NaN - not a number
+
+let result = 'the blog has ' + likes + ' likesssss';
+
+console.log(result);
+
+////////////////////////////////////////////////////
+// 00:46:16 - template strings
+
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+//let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
+//console.log(result);
+
+// template strings bzw. => template literal
+//let result = `The blog calles ${title} by ${author} has ${likes} likes`;
+//console.log(result);
+
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`; 
+ 
+console.log(html);
+
+let spanTemplate = $("#forTemplate");
+
+spanTemplate.append(html);
+
+
+
+//////////////////////////////////////////////////////////
+// 00:52:30 data type object - array
+ let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken'; // overwrite
+
+// console.log(ninjas);
+// console.log(ninjas[1]);
+
+
+// let ages = [20, 25, 30 ,35];
+
+// console.log(ages[2]);
+
+// let random = ['shaun', 'crystal', 30, 20];
+
+// console.log(random);
+
+//console.log(ninjas.length);
+
+// array methods
+
+// let result = ninjas.join(', ');
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.concat(['kendo', 'judo', 'mma']);
+let result = ninjas.push('yaba-daba-duu');  // some methods dont alter the original value BUT THİS ONE DO
+                                            // a DESTRUCTIVE METHOD
+
+result = ninjas.pop();
+console.log(result);
+console.log(ninjas);
+
+////////////////////////////////////////////////////////////
+// 01:02:20  NULL & undefined
+//let age;
+//let age = null;
+
+// console.log(age , age + 3, `the age is ${age}`);
+// console.log(true, false, 'true', 'false');
+
+// let email = 'attilakaradeniz@gmail.com';
+// let names = ['mario', 'luigi', 'thorn', 'shaun'];
+
+// let result = email.includes('@'); // returns true
+// let result = names.includes('luigi');
+
+// console.log(result);
+
+// comparison operators
+
+// let age = 25;
+
+// console.log(age == 25); // returns true
+// console.log(age == 30); // returns false
+// console.log(age != 30); // returns true
+// console.log(age != 30); // true
+// console.log(age > 24); // true
+// console.log(age <= 25); // true
+// console.log(age >= 25); // true
+
+// let name = 'ati';
+// console.log(name == 'ati');
+// console.log(name == 'Ati');
+
+// 01:15:03 loose comparison vs strict comparison
+let age = 25;
+
+// console.log(age == 25); // true
+// console.log(age == '25'); // true  // because of loose comparison
+
+// strict comaprison (different types cannot be aqual)
+console.log(age === '25'); // is age same value & type?
+
+
+////////////////////////////////////////////////////////////
+// 01:18:50  Type conversion
+let score = '100';
+
+// console.log(score + 1);
+// console.log(parseInt(score) + 1); // parseInt
+
+// console.log(typeof score);
+
+// score = Number(score);
+
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = Number('hellori');
+// let result = String(50);
+
+
+// truthy & falsy
+// let result = Boolean(100);  
+let result = Boolean(0); // falsy
+result = Boolean('0'); // truthy
+result = Boolean(''); // falsy
+
+
+console.log(result, typeof result);
+
+//////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 */
