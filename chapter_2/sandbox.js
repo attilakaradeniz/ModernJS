57133ef542595f7c6e07cae6ea70e71ffb29ea64
itsumogakusei <attilakaradeniz@gmail.com>
2020-03-08 12:41:48 +0100

    loop through for loop to creating a html template (with backtick)

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,18 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
     /////////
 
     let pTagJquery = $("#h1ID");
 
-    pTagJquery.css({position: "relative"});
-    pTagJquery.animate({left : 100});
+    pTagJquery.css({ position: "relative" });
+    pTagJquery.animate({ left: 100 });
 
-    pTagJquery.on("click", function(){
-        pTagJquery.animate({left: -100});
-        pTagJquery.animate({left : 100});
+    pTagJquery.on("click", function () {
+        for (let i = 0; i < 3; i++) {
+            pTagJquery.animate({ left: -100 });
+            pTagJquery.animate({ left: 100 });
+        }
     });
 
 
-    console.log(typeof pTagJquery);
+    //console.log(typeof pTagJquery); // gives "Object"
+
+    ////////////////////////////////////////////////
+    // // #3 00:00:00  Control Flow...
+    // for loops
+    // for(let i=0;i<5;i++){
+    //     console.log('in loop: ', i);
+    // }    
+    // console.log('loop is over!');
+
+    // const names = ['ati', 'kati', 'betty'];
+    // for(let i=0; i<names.length;i++){
+    //     console.log(i+1+'.' + ' element of name array is : ' + names[i]);
+
+    // }
+    const names = ['ati', 'kati', 'betty'];
+    for (let i = 0; i < names.length; i++) {
+        let html = `<div>${names[i]}</div>`;
+        console.log(html);
+    }
+
+
 
     /////////
 });
