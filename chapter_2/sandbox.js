b032ea749c2150fa30a20c39ed9c2983b506275f
itsumogakusei <attilakaradeniz@gmail.com>
2020-03-08 12:20:56 +0100

    goiing ahead #3 Control Flow...

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
     /////////
 
-    
+    let pTagJquery = $("#h1ID");
+
+    pTagJquery.css({position: "relative"});
+    pTagJquery.animate({left : 100});
+
+    pTagJquery.on("click", function(){
+        pTagJquery.animate({left: -100});
+        pTagJquery.animate({left : 100});
+    });
+
+
+    console.log(typeof pTagJquery);
 
     /////////
 });
