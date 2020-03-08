3320bb8a25409d414d634568dc2090a85a549ed8
itsumogakusei <attilakaradeniz@gmail.com>
2020-03-08 21:15:49 +0100

    functions & methods #4

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -13,29 +13,12 @@ $(document).ready(function () {
         }
     });
 
+    ///////////////////////////////////////////////
+    // #4 001810 regular functions & arrow functions
 
-    //console.log(typeof pTagJquery); // gives "Object"
-
-    ////////////////////////////////////////////////
-    // // #3 00:00:00  Control Flow...
-    // for loops
-    // for(let i=0;i<5;i++){
-    //     console.log('in loop: ', i);
-    // }    
-    // console.log('loop is over!');
-
-    // const names = ['ati', 'kati', 'betty'];
-    // for(let i=0; i<names.length;i++){
-    //     console.log(i+1+'.' + ' element of name array is : ' + names[i]);
-
-    // }
-    const names = ['ati', 'kati', 'betty'];
-    for (let i = 0; i < names.length; i++) {
-        let html = `<div>${names[i]}</div>`;
-        console.log(html);
-    }
+    
 
 
 
-    /////////
+
 });
